0a737f88c8cce927f67d78eb2699fa5ef20a66e2
AntonioRusan <antonrusan@gmail.com>
2024-02-18 23:46:50 +0300

    clean duplicate in tests

--- bot/src/test/java/edu/bot/BotCommandsTest.java
+++ bot/src/test/java/edu/bot/BotCommandsTest.java
@@ -8,11 +8,26 @@ import edu.java.bot.BotMessageProcessor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 public class BotCommandsTest {
+    private final Long chatId = 1L;
+
+    private Update getFakeUpdate(String fakeMessageText) {
+
+        Update fakeUpdate = Mockito.mock(Update.class);
+        Message fakeMessage = Mockito.mock(Message.class);
+        Chat fakeChat = Mockito.mock(Chat.class);
+
+        when(fakeChat.id()).thenReturn(chatId);
+
+        when(fakeMessage.chat()).thenReturn(fakeChat);
+        when(fakeMessage.text()).thenReturn(fakeMessageText);
+
+        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        return fakeUpdate;
+    }
 
     @Test
     @DisplayName("Проверка команды регистрации /start")
@@ -37,7 +52,7 @@ public class BotCommandsTest {
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
         SendMessage expectedResponse = new SendMessage(chatId, "Привет, " + userFirstName
-                + " " + userLastName + "!\nВы зарегистрированы!");
+            + " " + userLastName + "!\nВы зарегистрированы!");
 
         SendMessage actualResponse = botMessageProcessor.processMessage(fakeUpdate);
 
@@ -47,18 +62,7 @@ public class BotCommandsTest {
     @Test
     @DisplayName("Проверка команды /list - нет отслеживаемых ссылок")
     public void testListCommand() {
-        Long chatId = 1L;
-
-        Update fakeUpdate = Mockito.mock(Update.class);
-        Message fakeMessage = Mockito.mock(Message.class);
-        Chat fakeChat = Mockito.mock(Chat.class);
-
-        when(fakeChat.id()).thenReturn(chatId);
-
-        when(fakeMessage.chat()).thenReturn(fakeChat);
-        when(fakeMessage.text()).thenReturn("/list");
-
-        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        Update fakeUpdate = getFakeUpdate("/list");
 
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
@@ -72,22 +76,12 @@ public class BotCommandsTest {
     @Test
     @DisplayName("Проверка команды /help")
     public void testHelpCommand() {
-        Long chatId = 1L;
-
-        Update fakeUpdate = Mockito.mock(Update.class);
-        Message fakeMessage = Mockito.mock(Message.class);
-        Chat fakeChat = Mockito.mock(Chat.class);
-
-        when(fakeChat.id()).thenReturn(chatId);
-
-        when(fakeMessage.chat()).thenReturn(fakeChat);
-        when(fakeMessage.text()).thenReturn("/help");
-
-        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        Update fakeUpdate = getFakeUpdate("/help");
 
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
-        SendMessage expectedResponse = new SendMessage(chatId, "Список команд:\n" + String.join("\n", botMessageProcessor.commandDescriptionList));
+        SendMessage expectedResponse =
+            new SendMessage(chatId, "Список команд:\n" + String.join("\n", botMessageProcessor.commandDescriptionList));
 
         SendMessage actualResponse = botMessageProcessor.processMessage(fakeUpdate);
 
@@ -97,19 +91,9 @@ public class BotCommandsTest {
     @Test
     @DisplayName("Проверка команды /track - валидная ссылка")
     public void testTrackCommand() {
-        Long chatId = 1L;
         String url = "https://stackoverflow.com/";
 
-        Update fakeUpdate = Mockito.mock(Update.class);
-        Message fakeMessage = Mockito.mock(Message.class);
-        Chat fakeChat = Mockito.mock(Chat.class);
-
-        when(fakeChat.id()).thenReturn(chatId);
-
-        when(fakeMessage.chat()).thenReturn(fakeChat);
-        when(fakeMessage.text()).thenReturn("/track " + url);
-
-        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        Update fakeUpdate = getFakeUpdate("/track " + url);
 
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
@@ -123,19 +107,9 @@ public class BotCommandsTest {
     @Test
     @DisplayName("Проверка команды /track - невалидная ссылка")
     public void testTrackCommandNotValidUrl() {
-        Long chatId = 1L;
         String url = "not.a_url";
 
-        Update fakeUpdate = Mockito.mock(Update.class);
-        Message fakeMessage = Mockito.mock(Message.class);
-        Chat fakeChat = Mockito.mock(Chat.class);
-
-        when(fakeChat.id()).thenReturn(chatId);
-
-        when(fakeMessage.chat()).thenReturn(fakeChat);
-        when(fakeMessage.text()).thenReturn("/track " + url);
-
-        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        Update fakeUpdate = getFakeUpdate("/track " + url);
 
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
@@ -149,24 +123,14 @@ public class BotCommandsTest {
     @Test
     @DisplayName("Проверка команды /untrack - валидная ссылка")
     public void testUnTrackCommand() {
-        Long chatId = 1L;
         String url = "https://stackoverflow.com/";
 
-        Update fakeUpdate = Mockito.mock(Update.class);
-        Message fakeMessage = Mockito.mock(Message.class);
-        Chat fakeChat = Mockito.mock(Chat.class);
-
-        when(fakeChat.id()).thenReturn(chatId);
-
-        when(fakeMessage.chat()).thenReturn(fakeChat);
-        when(fakeMessage.text()).thenReturn("/untrack " + url);
-
-        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        Update fakeUpdate = getFakeUpdate("/untrack " + url);
 
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
         SendMessage expectedResponse = new SendMessage(chatId, "Прекращаем отслеживание изменений по ссылке: "
-                + url);
+            + url);
 
         SendMessage actualResponse = botMessageProcessor.processMessage(fakeUpdate);
 
@@ -176,19 +140,9 @@ public class BotCommandsTest {
     @Test
     @DisplayName("Проверка команды /untrack - невалидная ссылка")
     public void testUnTrackCommandNotValidUrl() {
-        Long chatId = 1L;
         String url = "not.a_url";
 
-        Update fakeUpdate = Mockito.mock(Update.class);
-        Message fakeMessage = Mockito.mock(Message.class);
-        Chat fakeChat = Mockito.mock(Chat.class);
-
-        when(fakeChat.id()).thenReturn(chatId);
-
-        when(fakeMessage.chat()).thenReturn(fakeChat);
-        when(fakeMessage.text()).thenReturn("/untrack " + url);
-
-        when(fakeUpdate.message()).thenReturn(fakeMessage);
+        Update fakeUpdate = getFakeUpdate("/untrack " + url);
 
         BotMessageProcessor botMessageProcessor = new BotMessageProcessor();
 
