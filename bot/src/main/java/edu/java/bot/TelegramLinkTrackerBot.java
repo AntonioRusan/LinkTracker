c3fcd259eb0f0d14df7a7cc12e8108c7606658fe
AntonioRusan <antonrusan@gmail.com>
2024-02-22 22:04:17 +0300

    removed application.properties and refactor TelegramBot creating

--- bot/src/main/java/edu/java/bot/TelegramLinkTrackerBot.java
+++ bot/src/main/java/edu/java/bot/TelegramLinkTrackerBot.java
@@ -1,29 +1,25 @@
 package edu.java.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
-import edu.java.bot.configuration.ApplicationConfig;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import static com.pengrad.telegrambot.UpdatesListener.CONFIRMED_UPDATES_ALL;
 
 @Service
 public class TelegramLinkTrackerBot {
 
     private final TelegramBot telegramBot;
-    private final ApplicationConfig applicationConfig;
     private final MessageProcessor messageProcessor = new BotMessageProcessor();
 
     @Autowired
-    public TelegramLinkTrackerBot(ApplicationConfig applicationConfig) {
-        this.applicationConfig = applicationConfig;
-        this.telegramBot = new TelegramBot(applicationConfig.telegramToken());
-        telegramBot.execute(messageProcessor.createCommandsMenu());
-        telegramBot.setUpdatesListener(updateList -> {
-            updateList.forEach(System.out::println);
-            updateList.forEach(update -> telegramBot.execute(messageProcessor.processMessage(update)));
-            return CONFIRMED_UPDATES_ALL;
-        });
+    public TelegramLinkTrackerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
 
+    @PostConstruct
+    private void initializeTelegramBot() {
+        telegramBot.execute(messageProcessor.createCommandsMenu());
+        telegramBot.setUpdatesListener(new TelegramLinkTrackerUpdateListener(telegramBot, messageProcessor));
     }
 
 }
