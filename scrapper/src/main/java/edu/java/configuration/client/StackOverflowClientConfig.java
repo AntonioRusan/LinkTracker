317407cf008767f4e4751207495f78cb45c113ae
AntonioRusan <antonrusan@gmail.com>
2024-04-21 20:53:40 +0300

    merging

--- scrapper/src/main/java/edu/java/configuration/client/StackOverflowClientConfig.java
+++ scrapper/src/main/java/edu/java/configuration/client/StackOverflowClientConfig.java
@@ -1,7 +1,6 @@
 package edu.java.configuration.client;
 
 import edu.java.configuration.ApplicationConfig;
-import edu.java.handlers.WebClientErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
